84bc9d454e8270fae764ccfcf8b0db05386dd2e5
vadevlife <vadevlife@gmail.com>
2021-12-04 08:59:59 -0300

    update desktop
    update desktop

---
+++ assets/js/cart.js
@@ -0,0 +1,13 @@
+
+
+function cart(){
+
+
+    var box = document.getElementById('cart');
+    var element = document.getElementById("myDIV");
+    
+    box.classList.toggle("toogle-cart");
+
+
+    console.log(box)
+}
