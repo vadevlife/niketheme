855cc87eddd5a5c852330cd942a70945da6529d3
vadevlife <vadevlife@gmail.com>
2021-12-09 08:36:53 -0300

    update

---
+++ assets/js/search.js
@@ -0,0 +1,18 @@
+
+function busca(){
+
+    var box = document.getElementById('busca')
+    box.classList.toggle("toogle");
+    console.log(box)
+}
+
+// function cart(){
+
+
+//     var box = document.getElementById('cart'); 
+    
+//     box.classList.toggle("toogle-cart");
+
+
+//     console.log(box)
+// }
